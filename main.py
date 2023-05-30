c496c0f3f50bc5344a01206899058868550c0914
anthem-landing18 <imokenpi2011@gmail.com>
2023-05-30 12:07:45 +0900

    fix

--- main.py
+++ main.py
@@ -86,9 +86,8 @@ class SlackChannels(Base):
         self.channel_name = channel_name
 
 def calc_execution_time():
-    now_datetime = datetime.datetime.now(pytz.utc).astimezone(JST_TIMEZONE)
-    start_time = now_datetime.replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=2)
-    end_time = now_datetime.replace(hour=23, minute=59, second=59, microsecond=999999) - datetime.timedelta(days=1)
+    start_time = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - datetime.timedelta(days=2)
+    end_time = datetime.datetime.now().replace(hour=23, minute=59, second=59, microsecond=999999) - datetime.timedelta(days=1)
     return start_time, end_time
 
 def write_csv(data, filename):
@@ -99,7 +98,9 @@ def write_csv(data, filename):
 
 
 def write_channel_data_for_csv(start_time, end_time, message_data):
-    timestr = start_time.strftime('%Y%m%d%H%M%S') + '-' + end_time.strftime('%Y%m%d%H%M%S')
+    start = JST_TIMEZONE.localize(start_time)
+    end = JST_TIMEZONE.localize(end_time)
+    timestr = start.strftime('%Y%m%d%H%M%S') + '-' + end.strftime('%Y%m%d%H%M%S')
     messages_csv = f"slack_messages_{timestr}.csv"
     reactions_csv = f"slack_reactions_{timestr}.csv"
 
